9bab194140344b92306f843341c472a7afec53f0
fatelei <fatelei@gmail.com>
2013-04-17 18:00:39 +0800

    init

--- sandbox/config.py
+++ sandbox/config.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
 #-*-coding: utf8-*-
 
+DENY_MODULES = ['sys', 'os']
 
+DENY_BUILTIN = ['compile', 'exec', 'type', 'dir', 'exit']
+
+DENY_CALL = ['system']
+
+RESOURCE_LIMIT = {
+    'memory': ''
+}
---
+++ sandbox/core.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*-coding: utf8-*-
+
+import ast
+
+class SdParse(ast.NodeVisitor):
+    def __init__(self):
+        pass
+
+    def visit_Import(self, stmt_import):
+        pass
+
+    def visit_Call(self, stmt_call):
+        pass
+
+    def visit_Exec(self, stmt_exec):
+        pass
+
+    def visit_ImportFrom(self, stmt_importfrom):
+        pass
+
+
